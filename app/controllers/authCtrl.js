ad2cb5382be37669f72ecfc31b1858e775e8c98f
Tamela Lerma <tlerma83@gmail.com>
2017-06-19 17:00:35 -0500

    awesome styling

--- app/controllers/authCtrl.js
+++ app/controllers/authCtrl.js
@@ -9,34 +9,43 @@ app.controller("AuthCtrl", function ($scope, $location, AuthFactory, DataFactory
         lastName: ""
     };
 
+    $scope.hideRegister = true;
 
     $scope.login = function () {
-        AuthFactory.loginUser($scope.auth)
-        .then(() =>{
+        if($scope.auth.email !== "" && $scope.auth.password !== "") {
+           AuthFactory.loginUser($scope.auth)
+            .then(() =>{
             console.log("user is logged in");
             $window.location.href = "#!/navigation";
-        });
+            });
+        }
+
     };
 
     $scope.register = function () {
-        AuthFactory.createUser({
-            email: $scope.auth.email,
-            password: $scope.auth.password
-        })
-        .then(function(newUser){
-            DataFactory.addNewUser({
-                uid: newUser.uid,
-                firtsName: $scope.auth.firstName,
-                lastName: $scope.auth.lastName
+        if($scope.auth.email !== "" && $scope.auth.password !== "" && $scope.auth.firstName !== "" && $scope.auth.lastName !== "") {
+            AuthFactory.createUser({
+                email: $scope.auth.email,
+                password: $scope.auth.password
+            })
+            .then(function(newUser){
+                DataFactory.addNewUser({
+                    uid: newUser.uid,
+                    firtsName: $scope.auth.firstName,
+                    lastName: $scope.auth.lastName
+                });
+            })
+            .then((userData) => {
+                $scope.login();
+                $window.location.href = "#!/navigation";
             });
-        })
-        .then((userData) => {
-            $scope.login();
-            $window.location.href = "#!/navigation";
-        });
+        }
     };
 
-
+    $scope.registerClick = function () {
+        $scope.hideLogin = true;
+        $scope.hideRegister = false;
+    };
 
 
 });
